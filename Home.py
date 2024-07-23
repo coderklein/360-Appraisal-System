5ff0edf842461b791aa0943771b4319b69baa5d1
coderklein <contactqwishi@gmail.com>
2024-07-23 10:55:10 +0300

    added authentication

--- Home.py
+++ Home.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from PIL import Image
 from pathlib import Path
+
+from streamlit_login_auth_ui.widgets import __login__
+
 # STREAMLIT PAGE CONFIG.
 
 st.set_page_config(page_title='360° Appraisal System', page_icon='⚖️⭐', layout="centered", initial_sidebar_state="collapsed")
@@ -28,4 +31,14 @@ logo = Image.open(logo)
 st.title("360° Appraisal System.")
 st.image(logo, use_column_width=True)
 
+__login__obj = __login__(auth_token = "courier_auth_token",
+                         company_name = "",
+                         width = 200, height = 250,
+                         logout_button_name = 'Logout', hide_menu_bool = False,
+                         hide_footer_bool = False,
+                         lottie_url = 'https://assets2.lottiefiles.com/packages/lf20_jcikwtux.json')
 
+is_logged_in = __login__obj.build_login_ui()
+
+if is_logged_in:
+    st.markown("The Application Begins here!")
